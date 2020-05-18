8313d8b731685568a606f13cecfd17155768deb1
Abhishek Dasgupta <abhidg@trenozoic.net>
2020-05-18 19:34:18 +0100

    Remove C-Tab used by browsers

--- set-up-gnome.sh
+++ set-up-gnome.sh
@@ -13,7 +13,7 @@ dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-down "['<Super><
 dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-left "['disabled']"
 dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-right "['disabled']"
 dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-up "['<Super><Ctrl>Left','<Super><Ctrl>Up']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-applications "['<Ctrl>Tab','<Alt>Tab']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-applications "['<Alt>Tab']"
 dconf write /org/gnome/desktop/wm/keybindings/switch-applications-backward "['<Shift><Alt>Tab','<Shift><Ctrl>Tab']"
 dconf write /org/gnome/desktop/wm/keybindings/switch-group "['<Ctrl>Above_Tab','<Alt>Above_Tab']"
 dconf write /org/gnome/desktop/wm/keybindings/switch-group-backward "['<Shift><Ctrl>Above_Tab','<Shift><Alt>Above_Tab']"
