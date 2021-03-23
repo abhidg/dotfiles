350649db52ce005eb293eaa44e4ff70fcdf20f0f
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2021-03-23 23:37:58 +0000

    update gnome settings

--- set-up-gnome.sh
+++ set-up-gnome.sh
@@ -3,6 +3,8 @@ dconf write /org/gtk/settings/file-chooser/sort-directories-first true
 dconf write /org/gnome/desktop/wm/keybindings/close "['<Super>q']"
 dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'gb+dvorak')]"
+gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
+# gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"  # fractional scaling
 
 gsettings set org.gnome.settings-daemon.plugins.media-keys volume-up "['<Alt>Up']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys volume-down "['<Alt>Down']"
@@ -12,6 +14,3 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys previous "['<Alt>Left
 gsettings set org.gnome.settings-daemon.plugins.media-keys mic-mute "['<Alt>Pause']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys volume-mute "['<Shift>Pause']"
 
-# <Alt>left click (right click) to move (resize) windows
-gsettings set org.gnome.desktop.wm.preferences mouse-button-modifier "<Alt>"
-gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
