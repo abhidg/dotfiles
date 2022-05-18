fb710ec0ba53bc3b5d1416789e86f8c4c218deca
Abhishek Dasgupta <abhidg@trenozoic.net>
2022-05-18 22:42:18 +0100

    fish: fix certificates path

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -3,4 +3,4 @@ fish_add_path ~/.nix-profile/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/bin
 set -g -x NIX_PROFILES "/nix/var/nix/profiles/default $HOME/.nix-profile"
-set -g -x NIX_SSL_CERT_FILE /etc/ssl/certs/ca-certficates.crt
+set -g -x NIX_SSL_CERT_FILE /etc/ssl/certs/ca-certificates.crt
